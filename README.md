1f1747de9f5e5877117c2c8d8a1c8e84f1b8f174
velbuntu <real.noah.lindner@gmail.com>
2025-10-22 20:59:51 +0200

    Added all files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# ptx-lists
-Adds lists to microbit.
+# MakeCode Extension for Scratch-Style Lists
+
+This extension for the BBC micro:bit adds blocks for creating and manipulating lists (arrays) in a way that is familiar to Scratch users.
+
+## How to Use
+1. Open the MakeCode editor for micro:bit.
+2. Click on "Extensions".
+3. Paste the URL of this GitHub repository into the search bar and import it.
---
+++ main.ts
@@ -0,0 +1,107 @@
+/**
+ * Custom blocks for list-like functionality inspired by Scratch.
+ */
+//% weight=100 color=#FF5733 icon="" block="Lists"
+namespace ScratchLists {
+
+    /**
+     * Adds an item to the end of a list.
+     * @param list The list to add the item to.
+     * @param item The item to add, eg: "hello"
+     */
+    //% block="add %item to %list"
+    //% list.shadow="variables_get" list.defl="myList"
+    export function addToList(item: any, list: any[]): void {
+        list.push(item);
+    }
+
+    /**
+     * Removes an item from a list at a specified index (position).
+     * @param list The list to remove the item from.
+     * @param index The zero-based index of the item to remove, eg: 0
+     */
+    //% block="remove item at #%index of %list"
+    //% list.shadow="variables_get" list.defl="myList"
+    //% index.min=0
+    export function removeFromList(index: number, list: any[]): void {
+        if (index >= 0 && index < list.length) {
+            list.splice(index, 1);
+        }
+    }
+
+    /**
+     * Returns the number of items in a list.
+     * @param list The list to get the length of.
+     */
+    //% block="length of %list"
+    //% list.shadow="variables_get" list.defl="myList"
+    export function listLength(list: any[]): number {
+        return list.length;
+    }
+
+    /**
+     * Inserts an item into a list at a specified index.
+     * @param list The list to insert the item into.
+     * @param index The zero-based index at which to insert the item, eg: 0
+     * @param item The item to insert, eg: "world"
+     */
+    //% block="insert %item at #%index of %list"
+    //% list.shadow="variables_get" list.defl="myList"
+    //% index.min=0
+    export function insertIntoList(item: any, index: number, list: any[]): void {
+        if (index >= 0 && index <= list.length) {
+            list.insertAt(index, item);
+        }
+    }
+
+    /**
+     * Replaces an item in a list at a specified index.
+     * @param list The list to modify.
+     * @param index The zero-based index of the item to replace, eg: 0
+     * @param item The new item.
+     */
+    //% block="replace item at #%index of %list with %item"
+    //% list.shadow="variables_get" list.defl="myList"
+    //% index.min=0
+    export function replaceItemInList(index: number, list: any[], item: any): void {
+        if (index >= 0 && index < list.length) {
+            list[index] = item;
+        }
+    }
+
+    /**
+     * Gets an item from a list at a specified index.
+     * @param list The list to get the item from.
+     * @param index The zero-based index of the item to get, eg: 0
+     */
+    //% block="item at #%index of %list"
+    //% list.shadow="variables_get" list.defl="myList"
+    //% index.min=0
+    export function getItemFromList(index: number, list: any[]): any {
+        if (index >= 0 && index < list.length) {
+            return list[index];
+        }
+        return undefined;
+    }
+
+    /**
+     * Checks if a list contains a specific item.
+     * @param list The list to check.
+     * @param item The item to look for.
+     */
+    //% block="%list contains %item"
+    //% list.shadow="variables_get" list.defl="myList"
+    export function listContains(list: any[], item: any): boolean {
+        return list.indexOf(item) != -1;
+    }
+
+    /**
+     * Removes all items from a list.
+     * @param list The list to clear.
+     */
+    //% block="delete all of %list"
+    //% list.shadow="variables_get" list.defl="myList"
+    export function deleteAllFromList(list: any[]): void {
+        list.length = 0;
+    }
+}
